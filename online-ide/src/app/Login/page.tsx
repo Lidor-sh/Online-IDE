056afa1de4b1c36679e47ccb196ae1753998b2d3
alexo02 <102466433+alexo02@users.noreply.github.com>
2024-04-27 10:15:55 +0300

    Added basic design for login/register screen

--- online-ide/src/app/Login/page.tsx
+++ online-ide/src/app/Login/page.tsx
@@ -1,35 +1,71 @@
 'use client';
-
 import React, { useState } from 'react';
 
-
-const LoginPage = ({ currentPage, setCurrentPage } : any) => (
-  <div>
-    <button onClick={() => setCurrentPage('SignUp')}>
-        go to SignUp
-    </button>
-  </div>
-);
-
-const SignUpPage = ({ currentPage, setCurrentPage } : any) => (
-  <div>
-    <button onClick={() => setCurrentPage('Login')}>
-        go to Login
-    </button>
-  </div>
-);
-
-const Page = ({ Component, pageProps } : any) => {
+const Page = () => {
   const [currentPage, setCurrentPage] = useState('Login');
+
+  const LoginPage = () => (
+    <div className="container">
+      <div className="login-container">
+        <h2>Sign in</h2>
+        <form className="form">
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input name="email" type="email" className="form-control" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input name="password" type="password" className="form-control" required />
+          </div>
+          <div className="form-group remember-me">
+            <input type="checkbox" id="remember" name="remember" />
+            <label htmlFor="remember">Remember me</label>
+          </div>
+          <button type="submit" className="button">Sign in</button>
+          <div className="forgot-password">
+            <a href="#">Forgot password?</a>
+          </div>
+        </form>
+        <div className="create-account">
+          Don't have an account? <button onClick={() => setCurrentPage('SignUp')} className="link">Create account</button>
+        </div>
+      </div>
+    </div>
+  );
+
+  const SignUpPage = () => (
+    <div className="container">
+      <div className="signup-container">
+        <h2>Create account</h2>
+        <form className="form">
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input name="name" type="text" className="form-control" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input name="email" type="email" className="form-control" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input name="password" type="password" className="form-control" required />
+          </div>
+          <button type="submit" className="button">Create account</button>
+        </form>
+        <div className="login-instead">
+          Already have an account? <button onClick={() => setCurrentPage('Login')} className="link">Sign in</button>
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <>
-      {currentPage === 'Login' ?
-       <LoginPage currentPage={currentPage} setCurrentPage={setCurrentPage} /> 
-       : 
-       <SignUpPage currentPage={currentPage} setCurrentPage={setCurrentPage} />}
+      {currentPage === 'Login' ? <LoginPage /> : <SignUpPage />}
     </>
-  ); 
+  );
 };
 
 export default Page;
+
+
