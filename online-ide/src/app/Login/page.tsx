301d27f66eb31458efd0efb9ebe02ff42e9251c4
Lidor-sh <lidorschool3@gmail.com>
2024-04-27 16:21:35 +0300

    Sign in/up page css

--- online-ide/src/app/Login/page.tsx
+++ online-ide/src/app/Login/page.tsx
@@ -1,87 +1,86 @@
 'use client';
 import React, { useState } from 'react';
+import Image from 'next/image';
+import backImage from "../images/background.jpg";
 
-<<<<<<< HEAD
-
-const LoginPage = ({ currentPage, setCurrentPage } : any) => (
-  <div>
-    <button onClick={() => setCurrentPage('SignUp')}>
-        go to SignUp!
-    </button>
-  </div>
-);
-
-const SignUpPage = ({ currentPage, setCurrentPage } : any) => (
-  <div>
-    <button onClick={() => setCurrentPage('Login')}>
-        go to Login!
-    </button>
-  </div>
-);
-
-const Page = ({ Component, pageProps } : any) => {
-=======
 const Page = () => {
->>>>>>> ab28898c25d7c420c72ee3b8d4f0d6d9f719c729
   const [currentPage, setCurrentPage] = useState('Login');
 
   const LoginPage = () => (
     <div className="container">
-      <div className="login-container">
-        <h2>Sign in</h2>
-        <form className="form">
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input name="email" type="email" className="form-control" required />
+      <div className="box">
+        <div className="login-container">
+          <h2 className='login-header'>Sign in</h2>
+          <form className="form">
+            <div className="form-group">
+              {/*<label htmlFor="email">Email</label>*/}
+              <input name="email" type="email" className="form-control" placeholder='Email' required />
+            </div>
+            <div className="form-group">
+              {/*<label htmlFor="password">Password</label>*/}
+              <input name="password" type="password" className="form-control" placeholder='Password' required />
+            </div>
+            <div className='login-footer'>
+              <div className="form-group remember-me">
+                <input type="checkbox" id="remember" name="remember" />
+                <label htmlFor="remember" className='remember-me'>Remember me</label>
+              </div>
+              <div className="forgot-password">
+                <a href="#" className="forgot-password">Forgot password?</a>
+              </div>
+            </div>
+            <button type="submit" className="button">Sign in</button>
+          </form>
+          <div className="create-account">
+            <button onClick={() => setCurrentPage('SignUp')} className="create-link">Don{"'"}t have an account?</button>
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input name="password" type="password" className="form-control" required />
+          <hr/>
+          <div className="social-login-container">
+            <button type="button" className="login-with-google-btn" >Sign in with Google</button>
+            <button type="button" className="login-with-github-btn" >Sign in with Github</button>
           </div>
-          <div className="form-group remember-me">
-            <input type="checkbox" id="remember" name="remember" />
-            <label htmlFor="remember">Remember me</label>
-          </div>
-          <button type="submit" className="button">Sign in</button>
-          <div className="forgot-password">
-            <a href="#" className="forgot-password">Forgot password?</a>
-          </div>
-        </form>
-        <div className="create-account">
-          Don't have an account? <button onClick={() => setCurrentPage('SignUp')} className="link">Create account</button>
         </div>
-      </div>
-      <div className="social-login-container">
-        {/* Social login buttons */}
+        <Image
+          src={backImage} 
+          alt="background"
+          className='background-image' />
       </div>
     </div>
   );
 
   const SignUpPage = () => (
     <div className="container">
-      <div className="signup-container">
-        <h2>Create account</h2>
-        <form className="form">
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input name="name" type="text" className="form-control" required />
+      <div className="box">
+        <div className="signup-container">
+          <h2 className='signup-header'>Sign up</h2>
+          <form className="form">
+            <div className="form-group">
+              {/*<label htmlFor="name">Name</label>*/}
+              <input name="name" type="text" className="form-control" placeholder='Username' required />
+            </div>
+            <div className="form-group">
+              {/*<label htmlFor="email">Email</label>*/}
+              <input name="email" type="email" className="form-control" placeholder='Email' required />
+            </div>
+            <div className="form-group">
+              {/*<label htmlFor="password">Password</label>*/}
+              <input name="password" type="password" className="form-control" placeholder='Password' required />
+            </div>
+            <button type="submit" className="button">Create Account</button>
+          </form>
+          <div className="login-instead">
+            <button onClick={() => setCurrentPage('Login')} className="signup-link">Already have an account?</button>
           </div>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input name="email" type="email" className="form-control" required />
+          <hr/>
+          <div className="social-login-container">
+            <button type="button" className="login-with-google-btn" >Sign in with Google</button>
+            <button type="button" className="login-with-github-btn" >Sign in with Github</button>
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input name="password" type="password" className="form-control" required />
-          </div>
-          <button type="submit" className="button">Create account</button>
-        </form>
-        <div className="login-instead">
-          Already have an account? <button onClick={() => setCurrentPage('Login')} className="link">Sign in</button>
         </div>
-      </div>
-      <div className="social-login-container">
-        {/* Social login buttons */}
+        <Image
+          src={backImage} 
+          alt="background"
+          className='background-image' />
       </div>
     </div>
   );
