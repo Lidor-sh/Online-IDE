1a949ccc1c1c1ad2b9e6c6a39a389bc375ad1bcc
Lidor-sh <lidorschool3@gmail.com>
2024-05-04 13:43:25 +0300

    Test Linkin

--- online-ide/src/lib/auth.ts
+++ online-ide/src/lib/auth.ts
@@ -20,7 +20,23 @@ export const authOptions = {
         }),
         LinkedInProvider({
             clientId: process.env.LINKEDIN_CLIENT_ID!,
-            clientSecret: process.env.LINKEDIN_CLIENT_SECRET!
+            clientSecret: process.env.LINKEDIN_CLIENT_SECRET!,
+            authorization: { 
+                params: { 
+                    scope: 'profile email openid',
+                    redirect_uri: 'http://localhost:3000/Login',
+                 } },
+            issuer: 'https://www.linkedin.com',
+            jwks_endpoint: "https://www.linkedin.com/oauth/openid/jwks",
+            async profile(profile) {
+                return {
+                    id: profile.sub,
+                    name: profile.name,
+                    firstname: profile.given_name,
+                    lastname: profile.family_name,
+                    email: profile.email
+                }
+            },
         }),
         DiscordProvider({
             clientId: process.env.DISCORD_CLIENT_ID!,
