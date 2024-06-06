806075c7a9d4287d9a714c36229611f93c213d9b
Lidor-sh <lidorschool3@gmail.com>
2024-06-06 21:25:42 +0300

    Fixed projects page a little bit

--- online-ide/src/app/Projects/page.tsx
+++ online-ide/src/app/Projects/page.tsx
@@ -1,40 +1,48 @@
 "use client";
 
 import { signOut, useSession } from "next-auth/react";
-import { redirect, useRouter } from "next/navigation";
-import { useRef } from "react";
-import { ScrollParallax } from "react-just-parallax";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
 
 export default function Page() {
   const { data: session } = useSession();
   const router = useRouter();
   const parallaxRef = useRef(null);
 
+  useEffect(() => {
+    if (!session) {
+      router.push("../Login");
+    }
+  }, [session, router]);
+
   if (!session) {
-    router.push("../Login");
-  } else {
-    return (
-      <>
-        <div
-          className="flex items-center justify-center h-[200vh] w-screen space-x-10"
-          ref={parallaxRef}
-        >
-          <img
-            src={session.user?.image ? session.user?.image : "../favicon.ico"}
-            className="rounded-full w-10"
-          />
-          <div className="text-gray-950">
-            Hi {session.user?.name}! your email address is:{" "}
-            {session.user?.email}
-          </div>
-          <button
-            className="text-white z-50 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-            onClick={() => signOut()}
-          >
-            Sign Out
-          </button>
-        </div>
-      </>
-    );
+    return null; // You can also show a loading spinner or a message here
   }
+
+  return (
+    <>
+      <div
+        className="flex items-center justify-center h-full w-full space-x-10"
+        ref={parallaxRef}
+      >
+        <Image
+          src={session.user?.image ? session.user?.image : "../favicon.ico"}
+          width={100}
+          height={100}
+          className="rounded-full w-10"
+          alt=""
+        />
+        <div className="text-gray-950">
+          Hi {session.user?.name}! your email address is: {session.user?.email}
+        </div>
+        <button
+          className="text-white z-50 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          onClick={() => signOut()}
+        >
+          Sign Out
+        </button>
+      </div>
+    </>
+  );
 }
